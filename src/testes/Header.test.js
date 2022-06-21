db694d82b48c1406ab49d4dc1a21c379e319ef6b
Thiago_Nery <nery.fisica@gmail.com>
2022-06-21 11:31:46 -0300

    Implementando os testes para o req 11

--- src/testes/Header.test.js
+++ src/testes/Header.test.js
@@ -1,4 +1,5 @@
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 import Foods from '../pages/Foods';
 import renderWithRouter from './helpers/renderWithRouter';
@@ -16,5 +17,14 @@ describe('Testa o componente Header', () => {
     expect(searchIcon).toBeDefined();
     const pageTitle = screen.getByRole('heading', { level: 1, name: /foods/i });
     expect(pageTitle).toBeDefined();
+  });
+
+  test('Verifica se ao clicar na imagem de perfil vai para a page profile', () => {
+    const { history } = renderWithRouter(<Foods />);
+    const profileIcon = screen.getByRole('button', {
+      name: /profile/i,
+    });
+    userEvent.click(profileIcon);
+    expect(history.location.pathname).toBe('/profile');
   });
 });
