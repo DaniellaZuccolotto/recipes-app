49f3f68a671742b59421445356103b21ed89911d
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-06-21 16:53:26 -0300

    [Grupo 2] Entrega Testes Requisito 10

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
+import renderWithRouter from './testes/helpers/renderWithRouter'
 
 test('Farewell, front-end', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/TRYBE/i);
-  expect(linkElement).toBeInTheDocument();
+  const { getByText } = renderWithRouter(<App />);
+  // const linkElement = getByText(/TRYBE/i);
+  // expect(linkElement).toBeInTheDocument();
 });
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,24 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
+import RecipeContext from '../provider/RecipesContext';
 
 function Header({ pageName, searchEnabled }) {
   const [enableSearchText, setEneableSearchText] = useState(false);
   const history = useHistory();
+  const { searchData, setSearchData } = useContext(RecipeContext);
+  const { search } = searchData;
 
-  const ProfileLink = () => {
+  const handleChange = ({ target: { value, name } }) => {
+    setSearchData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const ClickProfileLink = () => {
     history.push('/profile');
   };
 
@@ -16,35 +26,87 @@ function Header({ pageName, searchEnabled }) {
     if (enableSearchText === true) {
       return setEneableSearchText(false);
     }
+    setSearchData({
+      search: '',
+      filter: '',
+    });
     return setEneableSearchText(true);
   };
 
   return (
     <header>
-      <input
-        type="image"
-        src={ profileIcon }
-        alt="profile"
-        data-testid="profile-top-btn"
-        onClick={ ProfileLink }
-      />
-      <h1 data-testid="page-title">{ pageName }</h1>
-      {
-        searchEnabled && (<input
+      <div>
+        <input
           type="image"
-          src={ searchIcon }
-          alt="search"
-          data-testid="search-top-btn"
-          onClick={ ChangeSearchStatus }
-        />)
-      }
+          src={ profileIcon }
+          alt="profile"
+          data-testid="profile-top-btn"
+          onClick={ ClickProfileLink }
+        />
+        <h1 data-testid="page-title">{ pageName }</h1>
+        {
+          searchEnabled && (<input
+            type="image"
+            src={ searchIcon }
+            alt="open-search"
+            data-testid="search-top-btn"
+            onClick={ ChangeSearchStatus }
+          />)
+        }
+      </div>
       {
         enableSearchText && (
-          <input
-            type="text"
-            data-testid="search-input"
-            placeholder="Digite sua pesquisa"
-          />)
+          <div>
+            <input
+              type="text"
+              placeholder="Digite sua pesquisa"
+              name="search"
+              value={ search }
+              onChange={ handleChange }
+              data-testid="search-input"
+            />
+            <div>
+              <label htmlFor="ingredient">
+                <input
+                  type="radio"
+                  id="ingredient"
+                  name="filter"
+                  value="ingredient"
+                  onChange={ handleChange }
+                  data-testid="ingredient-search-radio"
+                />
+                Ingredient
+              </label>
+              <label htmlFor="name">
+                <input
+                  type="radio"
+                  id="name"
+                  name="filter"
+                  value="name"
+                  onChange={ handleChange }
+                  data-testid="name-search-radio"
+                />
+                Name
+              </label>
+              <label htmlFor="first-letter">
+                <input
+                  type="radio"
+                  id="first-letter"
+                  name="filter"
+                  value="first-letter"
+                  onChange={ handleChange }
+                  data-testid="first-letter-search-radio"
+                />
+                First Letter
+              </label>
+            </div>
+            <button
+              type="button"
+              data-testid="exec-search-btn"
+            >
+              Search
+            </button>
+          </div>)
       }
     </header>
   );
