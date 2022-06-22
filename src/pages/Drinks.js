0dd9fcdfbb1f0dfaba6b3225d8bd637a72bfa88b
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-06-22 16:54:38 -0300

    [Grupo 2] Entrega Requisitos 25 e 26 - Implementa renderização das 12 primeiras comidas e bebidas em suas respectivas páginas.

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Header from '../components/Header';
+import RecipeContext from '../provider/RecipesContext';
+import requestApi from '../helpers/requestApi';
 
 function Drinks() {
+  const { dataApi } = useContext(RecipeContext);
+  const [drinks, setDrinks] = useState([]);
+
+  const findDrinks = async () => {
+    const FOOD_LIST_LENGTH = 12;
+    const URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+    const drinkList = await requestApi(URL);
+    const first12Foods = await drinkList.drinks.slice(0, FOOD_LIST_LENGTH);
+    setDrinks(first12Foods);
+  };
+
+  useEffect(() => {
+    findDrinks();
+  }, []);
+
   return (
     <div>
       <Header pageName="Drinks" />
+      <section>
+        {
+          dataApi.length === 0 && drinks.map(({ strDrinkThumb, strDrink }, id) => (
+            <div key={ strDrink } data-testid={ `${id}-recipe-card` }>
+              <img
+                src={ strDrinkThumb }
+                alt={ strDrink }
+                data-testid={ `${id}-card-img` }
+              />
+              <p
+                data-testid={ `${id}-card-name` }
+              >
+                { strDrink }
+              </p>
+            </div>
+          ))
+        }
+      </section>
     </div>
   );
 }
--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Header from '../components/Header';
+import RecipeContext from '../provider/RecipesContext';
+import requestApi from '../helpers/requestApi';
 
 function Foods() {
+  const { dataApi } = useContext(RecipeContext);
+  const [foods, setFoods] = useState([]);
+
+  const findFoods = async () => {
+    const FOOD_LIST_LENGTH = 12;
+    const URL = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+    const foodList = await requestApi(URL);
+    const first12Foods = await foodList.meals.slice(0, FOOD_LIST_LENGTH);
+    setFoods(first12Foods);
+  };
+
+  useEffect(() => {
+    findFoods();
+  }, []);
+
   return (
-    <Header pageName="Foods" />
+    <main>
+      <Header pageName="Foods" />
+      <section>
+        {
+          dataApi.length === 0 && foods.map(({ strMealThumb, strMeal }, id) => (
+            <div key={ strMeal } data-testid={ `${id}-recipe-card` }>
+              <img
+                src={ strMealThumb }
+                alt={ strMeal }
+                data-testid={ `${id}-card-img` }
+              />
+              <p
+                data-testid={ `${id}-card-name` }
+              >
+                { strMeal }
+              </p>
+            </div>
+          ))
+        }
+      </section>
+    </main>
   );
 }
 
