bde7f6c30c851ca9f41244c769835a8d34ab8771
Fernando Bueno <fernando_jordi@hotmail.com>
2022-06-28 19:35:55 -0300

    salva receitas prontas ao local storage e cria mock para iniciar os testes

---
+++ src/testes/helpers/mockDoneRecipes.js
@@ -0,0 +1,39 @@
+export default doneRecipes = [
+  {
+    id: '52978',
+    type: 'food',
+    nationality: 'Turkish',
+    category: 'Side',
+    name: 'Kumpir',
+    image: 'https://www.themealdb.com/images/media/meals/mlchx21564916997.jpg',
+    doneDate: '28/5/2022',
+    tags: [
+      'SideDish',
+    ],
+  },
+  {
+    id: '52959',
+    type: 'food',
+    nationality: 'British',
+    category: 'Seafood',
+    name: 'Baked salmon with fennel & tomatoes',
+    image: 'https://www.themealdb.com/images/media/meals/1548772327.jpg',
+    doneDate: '28/5/2022',
+    tags: [
+      'Paleo,Keto,HighFat,Baking,LowCarbs',
+    ],
+  },
+  {
+    id: '17837',
+    type: 'drink',
+    nationality: '',
+    category: 'Ordinary Drink',
+    alcoholicOrNot: 'Alcoholic',
+    name: 'Adam',
+    image: 'https://www.thecocktaildb.com/images/media/drink/v0at4i1582478473.jpg',
+    doneDate: '28/5/2022',
+    tags: [
+      'Alcoholic,Holiday',
+    ],
+  },
+];
