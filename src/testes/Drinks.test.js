20153709156230f5ef6bba82685048d03f917336
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-06-28 19:20:54 -0300

    Alterações nos testes

--- src/testes/Drinks.test.js
+++ src/testes/Drinks.test.js
@@ -5,6 +5,91 @@ import App from '../App';
 import renderWithRouter from './helpers/renderWithRouter';
 
 describe('Testa a página Foods', () => {
+  test('Verificar se é chamado o endPoint correto da api de Drinks', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/drinks');
+    const searchIcon = screen.getByRole('button', {
+      name: /open-search/i,
+    });
+
+    const spy = jest.spyOn(global, 'fetch');
+
+    userEvent.click(searchIcon);
+    const searchText = screen.getByRole('textbox');
+    userEvent.type(searchText, 'vodka');
+    const searchBtn = screen.getByRole('button', { name: 'Search' });
+    userEvent.click(searchBtn);
+    expect(spy).toBeCalled();
+
+    const ingredientFilter = screen.getByRole('radio', { name: /ingredient/i });
+    userEvent.click(ingredientFilter);
+    userEvent.type(searchText, 'vodka');
+    userEvent.click(searchBtn);
+    expect(spy).toHaveBeenCalledWith(
+      'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=vodka',
+    );
+
+    const nameFilter = screen.getByRole('radio', { name: /name/i });
+    userEvent.click(nameFilter);
+    userEvent.type(searchText, 'Ace');
+    userEvent.click(searchBtn);
+    expect(spy).toHaveBeenCalledWith(
+      'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=Ace',
+    );
+
+    const firstLetterFilter = screen.getByRole('radio', { name: /first letter/i });
+    userEvent.click(firstLetterFilter);
+    userEvent.type(searchText, 'a');
+    userEvent.click(searchBtn);
+    expect(spy).toHaveBeenCalledWith(
+      'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a',
+    );
+
+    // referencia para pegar o teste de Alert 'https://stackoverflow.com/questions/53611098/how-can-i-mock-the-window-alert-method-in-jest'
+    const alert = jest.spyOn(global, 'alert');
+    userEvent.click(firstLetterFilter);
+    userEvent.type(searchText, 'ab');
+    userEvent.click(searchBtn);
+    const alertMsg = 'Your search must have only 1 (one) character';
+    expect(alert).toHaveBeenCalledWith(alertMsg);
+
+    spy.mockRestore();
+  });
+
+  test('Caso não encontre nenhuma receita, exibe a mensagem apropriada em Drinks',
+    () => {
+      const { history } = renderWithRouter(<App />);
+      history.push('/drinks');
+      const fetch = jest.spyOn(global, 'fetch');
+      const alert = jest.spyOn(global, 'alert');
+      const alertMsg = 'Sorry, we haven\'t found any recipes for these filters.';
+
+      const searchIcon = screen.getByRole('button', {
+        name: /open-search/i,
+      });
+      userEvent.click(searchIcon);
+
+      const nameFilter = screen.getByRole('radio', { name: /name/i });
+      const searchText = screen.getByRole('textbox');
+      const searchBtn = screen.getByRole('button', { name: 'Search' });
+
+      fetch.mockResolvedValue({
+        json: jest.fn().mockResolvedValue(null),
+      });
+
+      userEvent.type(searchText, 'lalala');
+      userEvent.click(nameFilter);
+      userEvent.click(searchBtn);
+
+      const WAIT_SEC = 3000;
+      setTimeout(() => {
+        expect(alert).toHaveBeenCalledWith(alertMsg);
+      }, WAIT_SEC);
+
+      fetch.mockRestore();
+      alert.mockRestore();
+    });
+
   test('Se os botões de categoria são rederizados', async () => {
     const { history } = renderWithRouter(<App />);
     history.push('/drinks');
@@ -73,37 +158,4 @@ describe('Testa a página Foods', () => {
 
     fetch.mockRestore();
   });
-
-  test('Caso não encontre nenhuma receita, exibe a mensagem apropriada em Drinks',
-    () => {
-      const { history } = renderWithRouter(<App />);
-      history.push('/drinks');
-
-      const fetch = jest.spyOn(global, 'fetch');
-
-      const alert = jest.spyOn(global, 'alert');
-      const alertMsg = 'Sorry, we haven\'t found any recipes for these filters.';
-
-      const searchIcon = screen.getByRole('button', {
-        name: /open-search/i,
-      });
-      userEvent.click(searchIcon);
-
-      const nameFilter = screen.getByRole('radio', { name: /name/i });
-      const searchText = screen.getByRole('textbox');
-      const searchBtn = screen.getByRole('button', { name: 'Search' });
-
-      fetch.mockResolvedValue({
-        json: jest.fn().mockResolvedValue(null),
-      });
-
-      userEvent.type(searchText, 'lalala');
-      userEvent.click(nameFilter);
-      userEvent.click(searchBtn);
-
-      const WAIT_SEC = 500;
-      setTimeout(() => {
-        expect(alert).toHaveBeenCalledWith(alertMsg);
-      }, WAIT_SEC);
-    });
 });
--- src/testes/Foods.test.js
+++ src/testes/Foods.test.js
@@ -1,11 +1,96 @@
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-// import userEvent from '@testing-library/user-event';
 import React from 'react';
 import App from '../App';
 import renderWithRouter from './helpers/renderWithRouter';
+import { mockFoodRecipe, mockFoodCategories } from './helpers/mockFoodResults';
 
 describe('Testa a página Foods', () => {
+  test('Verificar se é chamado o endPoint correto da api de Foods', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/foods');
+
+    const spy = jest.spyOn(global, 'fetch');
+
+    const searchIcon = screen.getByRole('button', {
+      name: /open-search/i,
+    });
+    userEvent.click(searchIcon);
+    const searchText = screen.getByRole('textbox');
+    userEvent.type(searchText, 'butter');
+    const searchBtn = screen.getByRole('button', { name: 'Search' });
+    userEvent.click(searchBtn);
+    expect(spy).toHaveBeenCalled();
+
+    const ingredientFilter = screen.getByRole('radio', { name: /ingredient/i });
+    userEvent.click(ingredientFilter);
+    userEvent.type(searchText, 'butter');
+    userEvent.click(searchBtn);
+    expect(spy).toHaveBeenCalledWith(
+      'https://www.themealdb.com/api/json/v1/1/filter.php?i=butter',
+    );
+
+    const nameFilter = screen.getByRole('radio', { name: /name/i });
+    userEvent.click(nameFilter);
+    userEvent.type(searchText, 'beef');
+    userEvent.click(searchBtn);
+    expect(spy).toHaveBeenCalledWith(
+      'https://www.themealdb.com/api/json/v1/1/search.php?s=beef',
+    );
+
+    const firstLetterFilter = screen.getByRole('radio', { name: /first letter/i });
+    userEvent.click(firstLetterFilter);
+    userEvent.type(searchText, 'a');
+    userEvent.click(searchBtn);
+    expect(spy).toHaveBeenCalledWith(
+      'https://www.themealdb.com/api/json/v1/1/search.php?f=a',
+    );
+
+    // referencia para pegar o teste de Alert 'https://stackoverflow.com/questions/53611098/how-can-i-mock-the-window-alert-method-in-jest'
+    jest.spyOn(global, 'alert').mockImplementation(() => {});
+    userEvent.click(firstLetterFilter);
+    userEvent.type(searchText, 'ab');
+    userEvent.click(searchBtn);
+    const alertMsg = 'Your search must have only 1 (one) character';
+    expect(global.alert).toHaveBeenCalledWith(alertMsg);
+
+    spy.mockRestore();
+  });
+
+  test('Caso não encontre nenhuma receita, exibe a mensagem apropriada',
+    () => {
+      const { history } = renderWithRouter(<App />);
+      history.push('/foods');
+      const fetch = jest.spyOn(global, 'fetch');
+      const alert = jest.spyOn(global, 'alert');
+      const alertMsg = 'Sorry, we haven\'t found any recipes for these filters.';
+
+      const searchIcon = screen.getByRole('button', {
+        name: /open-search/i,
+      });
+      userEvent.click(searchIcon);
+
+      const nameFilter = screen.getByRole('radio', { name: /name/i });
+      const searchText = screen.getByRole('textbox');
+      const searchBtn = screen.getByRole('button', { name: 'Search' });
+
+      fetch.mockResolvedValue({
+        json: jest.fn().mockResolvedValue(null),
+      });
+
+      userEvent.type(searchText, 'lalala');
+      userEvent.click(nameFilter);
+      userEvent.click(searchBtn);
+
+      const WAIT_SEC = 2000;
+      setTimeout(() => {
+        expect(alert).toHaveBeenCalledWith(alertMsg);
+      }, WAIT_SEC);
+
+      fetch.mockRestore();
+      alert.mockRestore();
+    });
+
   test('Se os botões de categoria são rederizados', async () => {
     const { history } = renderWithRouter(<App />);
     history.push('/foods');
@@ -43,17 +128,33 @@ describe('Testa a página Foods', () => {
 
   test('Verifica se exibe as 12 receitas', async () => {
     const { history } = renderWithRouter(<App />);
+    const fetch = jest.spyOn(global, 'fetch');
     history.push('/foods');
+    fetch.mockResolvedValueOnce(({
+      json: jest.fn().mockResolvedValue(mockFoodRecipe),
+    }))
+      .mockResolvedValueOnce({
+        json: jest.fn().mockResolvedValue(mockFoodCategories),
+      });
 
     const RECIPIES_LIST_LENGTH = 12;
     const recipiesImg = await screen.findAllByRole('img');
     expect(recipiesImg.length).toBe(RECIPIES_LIST_LENGTH);
+
+    fetch.mockRestore();
   });
 
   test('Verifica a funcionalidade dos filtros', async () => {
     const { history } = renderWithRouter(<App />);
     history.push('/foods');
     const fetch = jest.spyOn(global, 'fetch');
+
+    fetch.mockResolvedValueOnce(({
+      json: jest.fn().mockResolvedValue(mockFoodRecipe),
+    }))
+      .mockResolvedValueOnce({
+        json: jest.fn().mockResolvedValue(mockFoodCategories),
+      });
 
     const beefButton = await screen.findByRole('button', {
       name: /beef/i,
@@ -76,37 +177,4 @@ describe('Testa a página Foods', () => {
 
     fetch.mockRestore();
   });
-
-  test('Caso não encontre nenhuma receita, exibe a mensagem apropriada em Foods',
-    () => {
-      const { history } = renderWithRouter(<App />);
-      history.push('/foods');
-
-      const fetch = jest.spyOn(global, 'fetch');
-
-      const alert = jest.spyOn(global, 'alert');
-      const alertMsg = 'Sorry, we haven\'t found any recipes for these filters.';
-
-      const searchIcon = screen.getByRole('button', {
-        name: /open-search/i,
-      });
-      userEvent.click(searchIcon);
-
-      const nameFilter = screen.getByRole('radio', { name: /name/i });
-      const searchText = screen.getByRole('textbox');
-      const searchBtn = screen.getByRole('button', { name: 'Search' });
-
-      fetch.mockResolvedValue({
-        json: jest.fn().mockResolvedValue(null),
-      });
-
-      userEvent.type(searchText, 'lalala');
-      userEvent.click(nameFilter);
-      userEvent.click(searchBtn);
-
-      const WAIT_SEC = 500;
-      setTimeout(() => {
-        expect(alert).toHaveBeenCalledWith(alertMsg);
-      }, WAIT_SEC);
-    });
 });
